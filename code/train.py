ae84c0d2b68bbb77ac98e4c79bc7306c7be94b01
venus1004 <CharlesDevAlchemy@gmail.com>
2026-01-31 04:32:01 -0500

    change len

--- code/train.py
+++ code/train.py
@@ -28,7 +28,6 @@ class Trainer:
         else:
             np.random.shuffle(data)
             return data[:self.batch_size]
-
     def split_Xy(self, data) -> Tuple[np.ndarray, np.ndarray]:
         # board to input format (19x8x8)
         X = np.array([ChessEnv.state_to_input(i[0])[0] for i in data])
